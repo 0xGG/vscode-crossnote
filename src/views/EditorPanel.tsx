fc7b955d20716886146b2c722bfd99b56a577752
Yiyi Wang <shd101wyy@gmail.com>
2020-04-09 15:27:27 +0800

    feat: Added view container

--- src/views/EditorPanel.tsx
+++ src/views/EditorPanel.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "typeface-roboto";
+import "typeface-noto-sans-sc";
 import { ThemeProvider } from "@material-ui/styles";
 import "./editor/index";
 import { crossnoteTheme } from "./util/theme";
 import "./index.less";
 
 import Editor from "./components/Editor";
-
-import "typeface-roboto";
-import "typeface-noto-sans-sc";
 
 ReactDOM.render(
   <ThemeProvider theme={crossnoteTheme}>

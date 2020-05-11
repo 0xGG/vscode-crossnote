0d848a3b55287c6fdc2d196d75bfdd2cfbbcae61
Yiyi Wang <shd101wyy@gmail.com>
2020-05-11 15:41:06 +0800

    fix: Fixed OCR support

--- src/extension/WebviewConfig.ts
+++ src/extension/WebviewConfig.ts
@@ -2,6 +2,6 @@ import * as vscode from "vscode";
 export function getWebviewCSP(webview: vscode.Webview) {
   return `<meta
   http-equiv="Content-Security-Policy"
-  content="img-src 'self' data: https: ${webview.cspSource}; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://tessdata.projectnaptha.com https://gist.github.com blob: ${webview.cspSource}"
+  content="img-src 'self' data: https: ${webview.cspSource}; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://tessdata.projectnaptha.com https://gist.github.com https://unpkg.com blob: ${webview.cspSource}"
 />`;
 }
